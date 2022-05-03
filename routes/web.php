41906c03e1c373ac4f2ef1701fda3cdf02f483e7
Malak Krayem <70662779+MalakKrayem@users.noreply.github.com>
2022-05-03 23:38:32 +0300

    addassets

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('blog', function () {
+    return view('blog');
+});
+Route::get('contact', function () {
+    return view('contact');
+});
+Route::get('/single', function () {
+    return view('single');
 });
